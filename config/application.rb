e9a6b98cdb08de26e536d71170af8d847ba59875
Bennie Botha <bennie@moroku.com>
2019-02-13 10:52:42 +1100

    WIP

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative 'boot'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
