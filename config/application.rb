5c3ce2c40069bec9742920adf487481db8f33b40
Bennie Botha <bennie@moroku.com>
2019-02-13 10:30:38 +1100

    removed active model

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'active_model/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
